45daf52b170e052960a1a637abc8746591c5ba2f
Philippe Rasay <guerrerophilippe@gmail.com>
2019-03-22 16:06:17 +0100

    added phils components

---
+++ src/components/AddRestaurantComp/AddRestaurantComp.js
@@ -0,0 +1,117 @@
+import React, { Component } from "react"
+import {
+    Button,
+    Card,
+    CardBody,
+    legend,
+    Form,
+    FormGroup,
+    Label,
+    Input
+} from "reactstrap"
+
+class AddRestaurantComp extends Component {
+    constructor() {
+        super()
+        this.state = {
+            name: "",
+            address: "",
+            category: "",
+            description: ""
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+
+        const restaurant = {
+            name: this.state.restaurant,
+            address: this.state.address,
+            category: this.state.category,
+            description: this.state.description
+        }
+
+        console.log(restaurant)
+
+        const proxyurl = "https://cors-anywhere.herokuapp.com/"
+        fetch(proxyurl + "http://reviewsdb.herokuapp.com/restaurant", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name: this.state.restaurant,
+                address: this.state.address,
+                category: this.state.category,
+                description: this.state.description,
+                active: 1
+            })
+        })
+
+        alert("The restaurant is added")
+        window.location = "/Restaurant"
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <Card>
+                    <CardBody>
+                        <legend id="cardTitle">ADD A NEW RESTAURANT</legend>
+                        <Form onSubmit={this.handleSubmit}>
+                            <FormGroup>
+                                <Label for="restaurantName">
+                                    Restaurant Name
+                                </Label>
+                                <Input
+                                    type="text"
+                                    name="restaurant"
+                                    id="restaurantName"
+                                    onChange={this.handleChange}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="address">Address</Label>
+                                <Input
+                                    type="text"
+                                    name="address"
+                                    id="address"
+                                    onChange={this.handleChange}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="category">Category</Label>
+                                <Input
+                                    type="text"
+                                    name="category"
+                                    id="category"
+                                    onChange={this.handleChange}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="description">Description</Label>
+                                <Input
+                                    type="textarea"
+                                    name="description"
+                                    id="description"
+                                    onChange={this.handleChange}
+                                />
+                            </FormGroup>
+                            <Button type="submit" className="buttons">
+                                Add Restaurant
+                            </Button>
+                        </Form>
+                    </CardBody>
+                </Card>
+            </div>
+        )
+    }
+}
+
+export default AddRestaurantComp
---
+++ src/components/AddReviewComp/AddReviewComp.js
@@ -0,0 +1,100 @@
+import React, { Component } from "react"
+import StarRatings from "react-star-ratings"
+import "./AddReviewComp.css"
+import {
+    Button,
+    Card,
+    CardBody,
+    legend,
+    Form,
+    FormGroup,
+    Label,
+    Input
+} from "reactstrap"
+
+class AddReviewComp extends Component {
+    constructor() {
+        super()
+        this.state = {
+            reviewtext: "",
+            rating: 0
+        }
+        this.changeRating = this.changeRating.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    changeRating(newRating, name) {
+        this.setState({
+            rating: newRating
+        })
+    }
+
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+
+        const review = {
+            reviewtext: this.state.reviewtext,
+            rating: this.state.rating
+        }
+
+        const proxyurl = "https://cors-anywhere.herokuapp.com/"
+        fetch(proxyurl + "http://reviewsdb.herokuapp.com/review", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                reviewtext: this.state.reviewtext,
+                rating: this.state.rating,
+                active: 1
+            })
+        })
+
+        console.log(review)
+        window.location = "/Restaurant"
+    }
+
+    render() {
+        return (
+            <div className="container" id="formContainer">
+                <Card>
+                    <CardBody>
+                        <legend id="cardTitle">Your Review</legend>
+                        <Form onSubmit={this.handleSubmit}>
+                            <FormGroup>
+                                <Input
+                                    type="textarea"
+                                    name="reviewtext"
+                                    id="textreview"
+                                    style={{ height: 200 }}
+                                    onChange={this.handleChange}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <StarRatings
+                                    rating={this.state.rating}
+                                    starRatedColor="yellow"
+                                    starHoverColor="yellow"
+                                    changeRating={this.changeRating}
+                                    numberOfStars={5}
+                                    starDimension="40px"
+                                    name="rating"
+                                />
+                            </FormGroup>
+                            <Button type="submit" className="buttons">
+                                Add Review
+                            </Button>
+                        </Form>
+                    </CardBody>
+                </Card>
+            </div>
+        )
+    }
+}
+
+export default AddReviewComp
